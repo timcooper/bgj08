93bde482155bb5d6815cb23ad3e9761ff8156971
Tim Cooper <tjkoopa@gmail.com>
2014-10-18 16:09:20 +0100

    updated grunt task to include slopes and rebuilt gamejs

--- grunt/tasks/uglify.js
+++ grunt/tasks/uglify.js
@@ -3,7 +3,7 @@ var grunt = require('grunt');
 module.exports = {
 	build: {
 		files: {
-			'game/build/js/game.js': ['game/src/js/vendor/phaser.js', 'game/src/js/main.js']
+			'game/build/js/game.js': ['game/src/js/vendor/phaser.js', 'game/src/js/Arcade.slopes.js', 'game/src/js/main.js']
 		}
 	}
 };
